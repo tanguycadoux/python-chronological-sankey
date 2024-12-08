7866ad564ebdde9faf04610f5a60803f89ab1385
tanguycadoux <tanguycadoux04@gmail.com>
2024-12-08 23:09:25 +0100

    Changing JSON format for groups and dates, and adding quadratic bezier curves path between parents and childs

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
+import matplotlib.path as mpath
 import matplotlib as mpl
 import json
 import os
-from datetime import datetime, timedelta
-import numpy as np
+from datetime import datetime
 
 
 def get_dates_boundaries(diagram_data):
-    min_date = diagram_data[0]['date-begin']
-    max_date = diagram_data[0]['date-end']
+    min_date = diagram_data[0]['groups'][0]['date-begin']
+    max_date = diagram_data[0]['groups'][0]['date-end']
     for item in diagram_data[1:]:
-        min_date = min(min_date, item['date-begin'])
-        max_date = max(max_date, item['date-end'])
+        for group in item['groups']:
+            min_date = min(min_date, group['date-begin'])
+            max_date = max(max_date, group['date-end'])
     return (min_date, max_date)
 
 def get_rows_boundaries(diagram_data):
@@ -23,6 +25,14 @@ def get_rows_boundaries(diagram_data):
         max_row = max(max_row, item['row'])
     return (min_row, max_row)
 
+def get_list_of_groups(diagram_data):
+    groups = []
+    for item in diagram_data:
+        for group in item['groups']:
+            if group['name'] not in groups:
+                groups.append(group['name'])
+    return groups
+
 def load_json(filename):
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     with open(os.path.join(__location__, filename), 'r') as file:
@@ -30,14 +40,17 @@ def load_json(filename):
     return json_file
 
 
-TEXT_PADDING = 5
+TEXT_PADDING = 10
 EDGESIZE = 2
 ROW_HEIGHT_INCH = 3
 ROW_PADDING = .5
+COLORS = ['r', 'g']
 
 diagram_data = load_json('test-file.json')['chronological_sankey_data']
 (min_row, max_row) = get_rows_boundaries(diagram_data)
 (min_date, max_date) = get_dates_boundaries(diagram_data)
+
+list_of_groups = get_list_of_groups(diagram_data)
 
 with mpl.rc_context({
     'xtick.minor.size': 2,
@@ -45,11 +58,54 @@ with mpl.rc_context({
 
     fig, ax = plt.subplots(figsize=(12, (max_row - min_row)*ROW_HEIGHT_INCH))
 
-    for i, item in enumerate(diagram_data):
-        start = mdates.date2num(datetime.fromisoformat(item['date-begin']))
-        end = mdates.date2num(datetime.fromisoformat(item['date-end']))
-        ax.add_patch(plt.Rectangle((start, -item['row']), end-start, 1, lw=EDGESIZE, ec='k', label=item['group']))
-        ax.text(start + TEXT_PADDING, -item['row']+.5, item['name'], verticalalignment='center_baseline', fontsize=15)
+    for item in diagram_data:
+        row = item['row']
+        for i, group in enumerate(item['groups']):
+            start = mdates.date2num(datetime.fromisoformat(group['date-begin']))
+            end = mdates.date2num(datetime.fromisoformat(group['date-end']))
+            color = COLORS[list_of_groups.index(group['name'])]
+
+            if i > 0:
+                ax.add_patch(plt.Rectangle((start, -row), prev_end-start, 1, fc='k', alpha=.5))
+
+            ax.add_patch(plt.Rectangle((start, -row), end-start, 1, fc=color))
+            if i == 0:
+                ax.text(start + TEXT_PADDING, -row+.5, item['name'], verticalalignment='center_baseline', fontsize=15)
+
+            prev_end = end
+    
+    for parent in diagram_data:
+        number_of_childs = len(parent['childs'])
+        for i, child_name in enumerate(parent['childs']):
+            for child in diagram_data:
+                if child['name'] == child_name:
+                    row_begin_path = 1-parent['row']-i/number_of_childs
+                    row_end_path = 1-child['row']
+
+                    row_begin_height = 1/number_of_childs
+                    row_end_height = 1
+
+                    date_begin = parent['groups'][-1]['date-end']
+                    date_end = child['groups'][0]['date-begin']
+
+                    date_begin_path = mdates.date2num(datetime.fromisoformat(date_begin))
+                    date_end_path = mdates.date2num(datetime.fromisoformat(date_end))
+
+                    path_data = [
+                        (mpath.Path.MOVETO, (date_begin_path, row_begin_path)),
+                        (mpath.Path.CURVE4, ((date_begin_path + date_end_path)/2, row_begin_path)),
+                        (mpath.Path.CURVE4, ((date_begin_path + date_end_path)/2, row_end_path)),
+                        (mpath.Path.CURVE4, (date_end_path, row_end_path)),
+                        (mpath.Path.LINETO, (date_end_path, row_end_path-row_end_height)),
+                        (mpath.Path.CURVE4, ((date_begin_path + date_end_path)/2, row_end_path-row_end_height)),
+                        (mpath.Path.CURVE4, ((date_begin_path + date_end_path)/2, row_begin_path-row_begin_height)),
+                        (mpath.Path.CURVE4, (date_begin_path, row_begin_path-row_begin_height)),
+                        (mpath.Path.CLOSEPOLY, (date_begin_path, row_begin_path))
+                        ]
+
+                    codes, verts = zip(*path_data)
+                    patch = mpatches.PathPatch(mpath.Path(verts, codes), facecolor='k', alpha=.5)
+                    ax.add_patch(patch)
 
     ax.set_xlim([mdates.date2num(datetime.fromisoformat(min_date)),
                 mdates.date2num(datetime.fromisoformat(max_date))])
@@ -68,7 +124,5 @@ with mpl.rc_context({
     ax.spines.right.set_visible(False)
 
     ax.grid(True)
-
-    ax.legend()
 
     plt.show()
